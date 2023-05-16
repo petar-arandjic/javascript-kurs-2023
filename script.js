f76ca22da72bc83b6b4ec2e0ec084decf9376265
Petar <petar.arandjic>
2023-05-16 19:04:28 +0200

    new comment

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 // git status -- porvjerava da li ima promjena u projektu na vasoj lokalnoj masini
 // git add "ime fila" ili git add -A je za sve filove -- dodaje nove filove na projekat
 // git push -- ce da pusha na github
+// git pull -- ce da povuce nove promjene koje jos nismo povukli sa server
