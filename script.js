62b3d62820a08ab91027d01156712911f6936072
Petar <parandjic@gmail.com>
2023-05-16 19:03:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 // git clone "linkrepositorijuma" -- klonira repositorium na vasu masinu
 // git status -- porvjerava da li ima promjena u projektu na vasoj lokalnoj masini
 // git add "ime fila" ili git add -A je za sve filove -- dodaje nove filove na projekat
+// git push -- ce da pusha na github
