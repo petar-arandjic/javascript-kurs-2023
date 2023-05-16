c7a37ed44e8029fc075e57a9a37404cb21d57f5b
Petar <petar.arandjic>
2023-05-16 18:31:06 +0200

    Added scrpt.js file with git comments

---
+++ script.js
@@ -0,0 +1,3 @@
+// git clone "linkrepositorijuma" -- klonira repositorium na vasu masinu
+// git status -- porvjerava da li ima promjena u projektu na vasoj lokalnoj masini
+// git add "ime fila" ili git add -A je za sve filove -- dodaje nove filove na projekat
